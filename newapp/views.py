5481e1d69a552907931e420ec71ad1aa5ae7b761
richmondafari <afaririchmond1000@gmail.com>
2025-06-11 15:26:58 +0000

    feat(page): added the html template

--- newapp/views.py
+++ newapp/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def index(request):
-    return HttpResponse('<h1>Welcome to the New App!</h1><p>This is the index page of the new app.</p>')
-    
+    return render(request, 'index.html')
